3ca944c48b275d46b8170b3fe7a5d82434e00318
Ryan Robison <minisoftwareandgames@gmail.com>
2015-01-08 15:45:44 -0800

    New: Started adding code to work with the messaging view when a thread is clicked on. Fixed: N/A Broke: Doesn't add the view to the threads view anymore. Other: N/A

--- app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/Thread.java
+++ app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/Thread.java
@@ -1,12 +1,35 @@
 package com.buzzbridge.buzzbridge.buzzbridge;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Ryan on 12/19/2014.
  */
 public class Thread extends Fragment {
 
+    private RelativeLayout enterThreadView;
+    private View threadsLayoutRowView;
 
+    public View getThreadsLayoutRowView() {return this.threadsLayoutRowView;}
+
+    public static Thread newInstance(ViewGroup threadsContainer) {
+        Thread newThread = new Thread();
+
+        return newThread;
+    }
+
+    public Thread() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.thread, container, false);
+        return v;
+    }
 
 }
