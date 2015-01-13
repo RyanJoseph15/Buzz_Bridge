9647fa650e7a2b8cbea97fb19fc3d0278a737661
Ryan Robison <minisoftwareandgames@gmail.com>
2015-01-12 23:08:11 -0800

    New: Getters and Setters. Fixed: N/A Broke: N/A Other: Removed init method.

--- app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/MainActivity.java
+++ app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/MainActivity.java
@@ -2,11 +2,16 @@ package com.buzzbridge.buzzbridge.buzzbridge;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,19 +19,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    RelativeLayout mainActivityLayout;
-    static ThreadsFragment threadsFragment;
-    LinearLayout mainThreadsLayout;
+    private RelativeLayout mainActivityLayout;
+    private static ThreadsFragment threadsFragment;
+    private static LinearLayout mainThreadsLayout;
     private static ArrayList<Thread> threadsList;
-
-    public static ArrayList<Thread> getThreadsList() {return threadsList;}
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        threadsList = new ArrayList<Thread>();
 
-        init();
+        FragmentManager fmanager = getFragmentManager();
+        threadsFragment = ThreadsFragment.newThreadsFragment();
+        fmanager.beginTransaction().add(R.id.threads_fragment_container,
+                threadsFragment, "threadsFragment").commit();
+        mainActivityLayout = (RelativeLayout) findViewById(R.id.threads_fragment_container);
+        mainThreadsLayout = (LinearLayout) findViewById(R.id.threads_container);
 
     }
 
@@ -48,21 +59,18 @@ public class MainActivity extends Activity {
         } else if (id == R.id.action_add_thread) {
             Toast toast = Toast.makeText(getApplicationContext(), "new", Toast.LENGTH_SHORT);
             toast.show();
-            ThreadsFragment.addThread(threadsFragment, getApplicationContext(), mainThreadsLayout);
+            View parentView = inflater.inflate(R.layout.thread_fragment_row_view, mainThreadsLayout, false);
+            getThreadsFragment().addThread(threadsFragment, getApplicationContext(), parentView);
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void init() {
-
-        mainActivityLayout = (RelativeLayout) findViewById(R.id.threads_fragment_container);
-        mainThreadsLayout = (LinearLayout) findViewById(R.id.threads_container);
-        FragmentManager fmanager = getFragmentManager();
-        threadsList = new ArrayList<Thread>();
-
-        threadsFragment = ThreadsFragment.newThreadsFragment();
-        fmanager.beginTransaction().add(R.id.threads_fragment_container,
-                threadsFragment, "threadsFragment").commit();
+    // getters and setters
+    public static ArrayList<Thread> getThreadsList() {
+        return threadsList;
+    }
+    public static ThreadsFragment getThreadsFragment() {
+        return threadsFragment;
     }
 }
