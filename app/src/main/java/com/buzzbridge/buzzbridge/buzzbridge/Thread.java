db31e3401f40c259d8ddbdab2c3e014915fcc715
Ryan Robison <minisoftwareandgames@gmail.com>
2015-01-12 23:09:52 -0800

    New: View values for later. Getters and Setters. Moved attributes into this class rather than the ThreadsFragment Fixed: N/A Broke: N/A Other: N/A

--- app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/Thread.java
+++ app/src/main/java/com/buzzbridge/buzzbridge/buzzbridge/Thread.java
@@ -1,10 +1,15 @@
 package com.buzzbridge.buzzbridge.buzzbridge;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 /**
@@ -13,13 +18,15 @@ import android.widget.RelativeLayout;
 public class Thread extends Fragment {
 
     private RelativeLayout enterThreadView;
-    private View threadsLayoutRowView;
+    private RelativeLayout threadsLayoutRowView;
+    private EditText messageView;
+    private ImageView sendButton;
+    private ImageView attachmentButton;
 
-    public View getThreadsLayoutRowView() {return this.threadsLayoutRowView;}
-
-    public static Thread newInstance(ViewGroup threadsContainer) {
+    public static Thread newInstance(RelativeLayout threadsLayoutRowView, RelativeLayout enterThreadView) {
         Thread newThread = new Thread();
-
+        newThread.setThreadsLayoutRowView(threadsLayoutRowView);
+        newThread.setEnterThreadView(enterThreadView);
         return newThread;
     }
 
@@ -29,7 +36,23 @@ public class Thread extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.thread, container, false);
+        messageView = (EditText) v.findViewById(R.id.message_view);
+        sendButton = (ImageView) v.findViewById(R.id.send_button);
+        attachmentButton = (ImageView) v.findViewById(R.id.attachment);
         return v;
     }
+
+    // Getters and Setters
+    public void setThreadsLayoutRowView(RelativeLayout threadsLayoutRowView) {
+        this.threadsLayoutRowView = threadsLayoutRowView;
+    }
+    public RelativeLayout getThreadsLayoutRowView() {
+        return this.threadsLayoutRowView;
+    }
+    public EditText getMessageView() {return this.messageView;}
+    public ImageView getSendButton() {return this.sendButton;}
+    public ImageView getAttachmentButton() {return this.attachmentButton;}
+    public void setEnterThreadView(RelativeLayout enterThreadView) {this.enterThreadView = enterThreadView;}
+    public RelativeLayout getEnterThreadView() {return this.enterThreadView;}
 
 }
